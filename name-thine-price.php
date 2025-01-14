e76631653aac34114ca1d95f999c57725b935cd4
andrewklimek <andrew.klimek@gmail.com>
2025-01-14 14:34:45 -0500

    clean up head

--- name-thine-price.php
+++ name-thine-price.php
@@ -7,19 +7,6 @@ Description: Name Your Price / Donate plugin for WooCommerce
 Version:     0.2.0
 Author:      Andrew J Klimek
 Author URI:  https://github.com/andrewklimek
-License:     GPL2
-License URI: https://www.gnu.org/licenses/gpl-2.0.html
-
-Name Thine Price is free software: you can redistribute it and/or modify 
-it under the terms of the GNU General Public License as published by the Free 
-Software Foundation, either version 2 of the License, or any later version.
-
-Name Thine Price is distributed in the hope that it will be useful, but without 
-any warranty; without even the implied warranty of merchantability or fitness for a 
-particular purpose. See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with 
-Name Thine Price. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 
 TODO currently this plugin adds Name Your Price to every product.  Make a way to disable on a product-by-product basis
 */
